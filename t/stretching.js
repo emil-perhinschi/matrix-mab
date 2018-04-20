6d5fe9e4f995a3d1056a31a5027fc73a6a055546
Emil Nicolaie Perhinschi <emilper@gmail.com>
2018-04-20 22:56:03 +0300

    more tests, fixed NaN related bugs ... should remember to make sure my input is what I expect it to be

--- t/stretching.js
+++ t/stretching.js
@@ -16,3 +16,40 @@ const new_y = 100
 const result = matrix.stretch(orig, new_x, new_y)
 
 tap.equals(result.length, 100, "and stretched that matrix to width 10")
+
+// const test_image = "t/circles.pgm"
+// const fs = require("fs")
+// const PNG = require('pngjs').PNG;
+// const image_data = fs.readFileSync(
+//     test_image,
+//     "ascii"
+// )
+//
+// const image_matrix = matrix.pgm_to_matrix(image_data)
+// console.log(image_matrix.length, image_matrix[0].length)
+//
+//
+// debug_preview(image_matrix, 'input.png');
+// const stretched = matrix.stretch(image_matrix, 2000,2000)
+// debug_preview(stretched, 'ouput.png');
+//
+// function debug_preview(matrix, filename) {
+//
+//     var png = new PNG({
+//         width: matrix[0].length,
+//         height: matrix.length,
+//         filterType: -1
+//     });
+//
+//     for (var y = 0; y < png.height; y++) {
+//         for (var x = 0; x < png.width; x++) {
+//             var idx = (png.width * y + x) << 2;
+//             png.data[idx  ] = matrix[y][x];
+//             png.data[idx+1] = matrix[y][x];
+//             png.data[idx+2] = matrix[y][x];
+//             png.data[idx+3] = 255;
+//         }
+//     }
+//
+//     png.pack().pipe(fs.createWriteStream(filename));
+// }
