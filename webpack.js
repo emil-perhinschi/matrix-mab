31d9c485230471f5876249c3b8f9687e7f1244a2
Emil Nicolaie Perhinschi <emilper@gmail.com>
2018-05-07 00:14:44 +0300

    transpiling to es5 to please react

--- webpack.js
+++ webpack.js
@@ -17,7 +17,17 @@ var config = {
   },
   module: {
     rules: [
-    ]
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['babel-preset-es2015']
+                }
+            }
+        }
+    ],
   },
   resolve: {
     modules: [
